70f6f297684e11cf2052426927a2fd3425e10bcd
ayaf_bless <Blessambel1@gmail.com>
2022-07-09 17:52:24 -1200

    Create main.h

---
+++ main.h
@@ -0,0 +1,15 @@
+/**
+ * struct print - struct for printer functions
+ * @type_arg: identifier
+ * @f: pointer to a printer functions
+ *
+ * Description: struct that stores pointers to a
+ * printer functions.
+ */
+typedef struct print
+{
+	char *type_arg;
+	int (*f)(va_list, char *, unsigned int);
+} print_t;
+
+int _printf(const char *format, ...);
